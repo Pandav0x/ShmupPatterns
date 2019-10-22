a496ee9c3b86d0bc84f569bc9d68a4ff77587a8f
Panda <pandav0x@hotmail.fr>
2019-10-22 23:07:28 +0200

    Objects + movement

--- scripts/game.js
+++ scripts/game.js
@@ -2,7 +2,6 @@
 
 var redirectTo = "./redirect.html";
 var spriteScale = 1.5;
-var bulletNumber = 100;
 
 var gameConfig = {
     type: Phaser.AUTO,
@@ -44,35 +43,149 @@ function create ()
     this.physics.world.setBounds = bounds;
 
     //enemy
-    this.enemy = this.add.sprite((gameConfig.width/2), (gameConfig.height/2), 'enemy').setScale(spriteScale);
-
-    this.bullets = [];
-    for(let i = 0; i < bulletNumber; i++){
-        this.bullets[i] = new Bullet();
-        this.bullets[i].angle = i * 10;
-        this.bullets[i].image = this.add.sprite(this.bullets[i].x, this.bullets[i].y, 'bullet');
-
-    }
+    this.enemy = new Enemy(this, (gameConfig.width/2), (gameConfig.height/2));
 
 }
 
 function update ()
 {
-    for(let i = 0; i < bulletNumber; i++){
-       this.bullets[i].move();
-        console.log(i + ":{x:" + this.bullets[i].x + ",y:" + this.bullets[i].y + "}");
+    this.enemy.shoot();
+
+    var cursors = this.input.keyboard.createCursorKeys();
+    if(cursors.up.isDown){
+        this.enemy.moveUp();
+        console.log("u");
+    }
+    else if(cursors.down.isDown){
+        this.enemy.moveDown();
+    }
+    else if (cursors.left.isDown){
+        this.enemy.moveLeft();
+    }
+    else if (cursors.right.isDown){
+        this.enemy.moveRight();
+    }
+}
+
+class Enemy{
+    context = null;
+    firerate = 500;
+    sprite = null;
+    moveSpeed = 5;
+    coordinate = {
+        x: null,
+        y: null
+    };
+    bullets;
+
+    constructor(context, coordX, coordY){
+        this.context = context;
+        this.coordinate.x = coordX;
+        this.coordinate.y = coordY;
+        this.bullets = new BulletCollection(context, this.coordinate.x, this.coordinate.y);
+        this.sprite = this.context.add.sprite(this.coordinate.x, this.coordinate.y, 'enemy').setScale(spriteScale);
+        this.draw();
+        this.behavior();
+    }
+    shoot = function() {
+        this.bullets.moveAll();
+    }
+    draw = function(){
+        this.bullets.setPosition(this.coordinate.x, this.coordinate.y);
+        this.sprite.x = this.coordinate.x;
+        this.sprite.y = this.coordinate.y;
+    }
+    behavior = function() {
+        var bullets =  this.bullets;
+        this.context.time.addEvent({
+            delay: this.firerate,
+            callback: function(){
+                bullets.createBullets();
+            },
+            //repeat: 3
+            loop: true
+        });
+    }
+    moveUp = function(){
+        this.coordinate.y -= this.moveSpeed;
+        this.draw();
+    };
+    moveDown = function(){
+        this.coordinate.y += this.moveSpeed;
+        this.draw();
+    };
+    moveLeft = function(){
+        this.coordinate.x -= this.moveSpeed;
+        this.draw();
+    };
+    moveRight = function(){
+        this.coordinate.x += this.moveSpeed;
+        this.draw();
+    };
+}
+
+class BulletCollection{
+    context = null;
+    bulletNumber = 100;
+    bullets = [];
+    origin = {
+        x: null,
+        y: null
+    };
+
+    constructor(context, origX, origY) {
+        this.context = context;
+        this.origin.x = origX;
+        this.origin.y = origY;
+    }
+    moveAll = function(){
+        if(this.bullets.length === 0)
+            return;
+        this.bullets.forEach(function(bullet, index, parent){
+            if(!bullet.move())
+                parent.splice(index, 1);
+                console.log()
+        });
+    }
+    createBullets = function(){
+        var bulletNumber = this.bullets.length | 0;
+        for(let i = bulletNumber; i < (this.bulletNumber + bulletNumber); i++){
+            this.bullets[i] = new Bullet(this.context, this.origin.x, this.origin.y);
+            this.bullets[i].angle = i * 10;
+            this.bullets[i].sprite = this.context.add.sprite(this.bullets[i].x, this.bullets[i].y, 'bullet');
+        }
+    }
+    setPosition = function(newX, newY) {
+        this.origin.x = newX;
+        this.origin.y = newY;
     }
 }
 
 class Bullet{
-    x = (gameConfig.width/2);
-    y = (gameConfig.height/2);
+    context = null;
+    sprite = null;
     speed = 0.05;
+    position = {
+        x: null,
+        y: null
+    };
+
+    constructor(context, origX, origY){
+        this.context = context;
+        this.position.x = origX;
+        this.position.y = origY;
+    }
     move = function(){
-        this.x += (90*Math.cos(this.angle))*this.speed;
-        this.y += (90*Math.sin(this.angle))*this.speed;
-        this.image.x = this.x;
-        this.image.y = this.y;
+        this.position.x += (90*Math.cos(this.angle))*this.speed;
+        this.position.y += (90*Math.sin(this.angle))*this.speed;
+        if(this.position.x > gameConfig.width || this.position.x < 0 || this.position.y > gameConfig.height || this.position.y < 0)
+        {
+            this.sprite.destroy();
+            return false;
+        }
+        this.sprite.x = this.position.x;
+        this.sprite.y = this.position.y;
+        return true;
     }
 
 }
